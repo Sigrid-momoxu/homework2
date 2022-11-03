fdbe104528647fba40f131284145689a36cda2b6
Sigrid <sisi@SigirddeMacBook-Pro.local>
2022-11-03 14:51:05 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+fin = open('/Users/sisi/Desktop/cme.20210709.c.pa2', 'r')
